6a6648d8279284308c3ef41f1dbe637943114d59
Jproger <webscripting@ya.ru>
2018-07-13 11:05:59 +0300

    Build App

---
+++ app/src/main/java/com/jproger/hf6/DrinkCategoryActivity.java
@@ -0,0 +1,20 @@
+package com.jproger.hf6;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends ListActivity{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
+    }
+}
--- app/src/main/java/com/jproger/hf6/MainActivity.java
+++ app/src/main/java/com/jproger/hf6/MainActivity.java
@@ -2,6 +2,11 @@ package com.jproger.hf6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(MainActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
+
