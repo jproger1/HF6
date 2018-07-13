a8e8ecbcb5c3ef996632caac71a96a12eb709254
Jproger <webscripting@ya.ru>
2018-07-13 12:08:56 +0300

    Add Drink Activity

--- app/src/main/java/com/jproger/hf6/DrinkCategoryActivity.java
+++ app/src/main/java/com/jproger/hf6/DrinkCategoryActivity.java
@@ -1,7 +1,9 @@
 package com.jproger.hf6;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,5 +18,12 @@ public class DrinkCategoryActivity extends ListActivity{
                 android.R.layout.simple_list_item_1,
                 Drink.drinks);
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
